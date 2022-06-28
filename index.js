e3cba2c237a625fa4a5f7d34df2d70db6878ec18
Grant Ours <grant.ours55@gmail.com>
2022-06-28 16:17:55 -0600

    commit tree ab0c2591838c2c7bd8e99f9c7ccf163cab848e66

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your solution in this file!
+customerName = "bob";
+const leastFavoriteCustomer = "gerry"
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer(newCustomer) {
+    bestCustomer = newCustomer;
+}
+
+function changeLeastFavoriteCustomer(LFC) {
+    leastFavoriteCustomer = LFC;
+}
